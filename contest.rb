add996139dbaaf010a3efe35b857c886a9c98e91
Scott Chacon <schacon@gmail.com>
2009-06-29 08:01:50 -0700

    remove testing

--- contest.rb
+++ contest.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+
+class Entry
+  include DataMapper::Resource
+ 
+  # The Serial type provides auto-incrementing primary keys
+  property :id,         Serial
+  # ...or pass a :key option for a user-set key like the name of a user:
+  property :name,       String, :key => true
+ 
+  property :title,      String
+  property :body,       Text
+  property :created_at, DateTime
+end
+
 # post receive handler
 get '/hook' do
 end
@@ -11,8 +26,4 @@ end
 
 # individual project data api
 get '/p/:user/:repo' do
-end
-
-get '/env' do
-  ENV.inspect
 end
