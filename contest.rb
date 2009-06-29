1e15f20ea6615aba23cf79567ac2f5b91ceb73a1
Scott Chacon <schacon@gmail.com>
2009-06-29 13:18:50 -0700

    beginning of post receive

--- contest.rb
+++ contest.rb
@@ -1,29 +1,91 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'open-uri'
+require 'json'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+## -- DATABASE STUFF --
 
-class Entry
-  include DataMapper::Resource
- 
-  # The Serial type provides auto-incrementing primary keys
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/local.db")
+
+class ContestEntry
+  include DataMapper::Resource 
   property :id,         Serial
-  # ...or pass a :key option for a user-set key like the name of a user:
-  property :name,       String, :key => true
- 
-  property :title,      String
-  property :body,       Text
-  property :created_at, DateTime
+  property :name,       String, :key => true 
+  property :owner,      String
+  property :email,      String
+  property :description, String
+  property :homepage,    String
+  property :entered,    DateTime
+  property :highscore,  Integer
+end
+
+class Score
+  include DataMapper::Resource 
+  belongs_to :contest_entry
+  property :id,       Serial  
+  property :ref,      String
+  property :sha,      String
+  property :entered,  DateTime
+  property :score,    Integer
+end
+
+DataMapper.auto_upgrade!
+
+## -- WEBSITE STUFF --
+
+get '/' do
+  # frontpage
 end
 
 # post receive handler
-get '/hook' do
+post '/' do
+  push = JSON.parse(params[:payload])
+  
+  repo      = push['repository']
+  repo_name = repo['name']
+  owner     = repo['owner']['name']
+  after     = push['after']
+  
+  # get or create the entry
+  entry = ContestEntry.find(:name => repo_name, :owner => owner)
+  if !entry
+    entry = ContestEntry.new
+    entry.attributes = {:name => repo_name, :owner => owner}
+    entry.save
+    # email to congratulate for joining?
+  end
+
+  # read the results
+  raw = "http://github.com/#{repo_name}/#{owner}/raw/#{after}/results.txt"
+  results = open(raw) do |f|
+    f.read
+  end
+
+  if results
+    key = JSON.parse(File.read('key.json'))
+    score = 0
+    results.split("\n").each do |guess|
+      (uid, rids) = guess.split(':')
+      next if !key[uid]
+      rids = rids.split(',')
+      if rids.include? key[uid]
+        score += 1
+      end
+    end
+    if score > 0
+      score = Score.new
+      score.contest_entry = entry
+      score.score = score
+      score.save
+    end
+  end
 end
 
 # leaderboard api
 get '/leaderboard' do
 end
 
-# individual project data api
+# individual project data
 get '/p/:user/:repo' do
 end
