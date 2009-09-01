bd25796acd220257b6e2f66d5b90e6b07e558443
Scott Chacon <schacon@ubuntu.(none)>
2009-09-01 08:28:23 -0700

    contest is over

--- contest.rb
+++ contest.rb
@@ -9,36 +9,36 @@ require 'json'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/local.db")
 
 class ContestEntry
-  include DataMapper::Resource 
+  include DataMapper::Resource
   has n, :scores
   has n, :pushes
   property :id,         Serial
-  property :name,       String, :key => true 
+  property :name,       String, :key => true
   property :owner,      String
   property :email,      String
   property :description, String
   property :homepage,    String
   property :entered,    DateTime
   property :highscore,  Integer
-  
+
   def nwo
     self.owner + '/' + self.name
   end
 end
 
 class Score
-  include DataMapper::Resource 
+  include DataMapper::Resource
   belongs_to :contest_entry
   belongs_to :push
-  property :id,       Serial  
+  property :id,       Serial
   property :score,    Integer
 end
 
 class Push
-  include DataMapper::Resource 
+  include DataMapper::Resource
   has 1, :score
   belongs_to :contest_entry
-  property :id,       Serial  
+  property :id,       Serial
   property :ref,      String
   property :sha,      String
   property :results_sha, String, :index => true
@@ -83,89 +83,6 @@ end
 
 # post receive handler
 post '/' do
-  push = JSON.parse(params[:payload])
-  
-  repo      = push['repository']
-  repo_name = repo['name']
-  owner     = repo['owner']['name']
-  after     = push['after']
-  
-  # get or create the entry
-  entry = ContestEntry.first(:name => repo_name, :owner => owner)
-  if !entry
-    entry = ContestEntry.new
-    entry.attributes = {:name => repo_name, :owner => owner, :entered => Time.now()}
-    entry.save
-    # email to congratulate for joining?
-  end
-  entry.homepage = repo['homepage'][0, 40]
-  entry.description = repo['description'][0, 40]
-  entry.email = repo['owner']['email']
-
-  # save the push information
-  pu = Push.new
-  pu.sha = after
-  pu.ref = push['ref']
-  pu.entered = Time.now()
-  pu.save
-  entry.pushes << pu
-  entry.save
-
-  # look for a changed file
-  new_results = false
-  tree_url = "http://github.com/api/v2/json/tree/show/#{owner}/#{repo_name}/#{after}"
-  tree = open(tree_url) do |f|
-    f.read
-  end
-
-  new_tree = JSON.parse(tree)
-  new_tree['tree'].each do |f|
-    if f['name'] == 'results.txt'
-      if !Push.first(:results_sha => f['sha'])
-        new_results = true
-        pu.message = 'processing new results.txt file'
-      else
-        pu.message = 'no new results.txt file'
-      end
-      pu.results_sha = f['sha']
-      pu.save
-    end
-  end
-  
-  if new_results
-    # read the results
-    raw = "http://github.com/#{owner}/#{repo_name}/raw/#{after}/results.txt"
-    results = open(raw) do |f|
-      f.read
-    end
-
-    if results
-      key = JSON.parse(File.read('key.json'))
-      score = 0
-    
-      # verify that it is the right format (add to error if not)
-      results.split("\n").each do |guess|
-        (uid, rids) = guess.split(':')
-        next if !key[uid]
-        next if !rids
-        rids = rids.split(',')[0, 10] # verify that each entry only has up to 10 guesses
-        if rids.include? key[uid]
-          key.delete uid # verify that each entry is only there once
-          score += 1
-        end
-      end
-
-      sc = Score.new
-      sc.score = score
-      sc.push = pu
-      sc.save
-      entry.scores << sc
-      hs = entry.highscore || 0
-      if score > hs
-        entry.highscore = score
-      end
-      entry.save
-    end
-    
-  end
+  # contest over
 end
+
